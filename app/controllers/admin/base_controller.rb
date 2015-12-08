b8eb6f415c4768c3fc3167bec12d310143dc10ac
Geoffrey Marcelino <geoffrey24@gmail.com>
2015-12-08 18:25:41 +0000

    create project and basic login flow

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,13 @@
+class Admin::BaseController < ApplicationController
+  helper_method :current_user
+
+  def index
+  end
+
+	private
+
+	def current_user
+	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+
+end
